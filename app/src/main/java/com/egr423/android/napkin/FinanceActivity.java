9708ab9c0b75b825be64c067cc5a3c1b16def0e6
OceanDL <oceandouglas.lee@calbaptist.edu>
2018-11-07 16:23:23 -0800

    Activity calculator format

--- app/src/main/java/com/egr423/android/napkin/FinanceActivity.java
+++ app/src/main/java/com/egr423/android/napkin/FinanceActivity.java
@@ -10,7 +10,7 @@ import android.view.View;
 import android.widget.GridLayout;
 import android.widget.Toast;
 
-public class LifestyleActivity extends AppCompatActivity {
+public class FinanceActivity extends AppCompatActivity {
     GridLayout mainGrid;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,12 +31,12 @@ public class LifestyleActivity extends AppCompatActivity {
                     if (cardView.getCardBackgroundColor().getDefaultColor() == -1) {
                         //Change background color
                         cardView.setCardBackgroundColor(Color.parseColor("#FF6F00"));
-                        Toast.makeText(LifestyleActivity.this, "State : True", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(FinanceActivity.this, "State : True", Toast.LENGTH_SHORT).show();
 
                     } else {
                         //Change background color
                         cardView.setCardBackgroundColor(Color.parseColor("#FFFFFF"));
-                        Toast.makeText(LifestyleActivity.this, "State : False", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(FinanceActivity.this, "State : False", Toast.LENGTH_SHORT).show();
                     }
                 }
             });
@@ -44,15 +44,21 @@ public class LifestyleActivity extends AppCompatActivity {
     }
 
     private void setSingleEvent(GridLayout mainGrid) {
+        //Loop all child item of Main Grid
+        for (int i = 0; i < mainGrid.getChildCount(); i++) {
+            //You can see , all child item is CardView , so we just cast object to CardView
+            CardView cardView = (CardView) mainGrid.getChildAt(i);
+            final int finalI = i;
+            cardView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
 
-        CardView cardView = (CardView) mainGrid.getChildAt(0);
-        cardView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(LifestyleActivity.this,CalculatorActivity.class);
-                startActivity(intent);
-            }
-        });
+                    Intent intent = new Intent(FinanceActivity.this,CalculatorActivity.class);
+                    startActivity(intent);
+
+                }
+            });
+        }
     }
 }
 
