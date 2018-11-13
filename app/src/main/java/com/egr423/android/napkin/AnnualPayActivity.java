079ea96f6f5e3396f0894eb152519aa399c45199
OceanDL <oceandouglas.lee@calbaptist.edu>
2018-11-13 14:19:11 -0800

    Fix focus mode so keyboard no longer pops up

--- app/src/main/java/com/egr423/android/napkin/AnnualPayActivity.java
+++ app/src/main/java/com/egr423/android/napkin/AnnualPayActivity.java
@@ -2,11 +2,14 @@ package com.egr423.android.napkin;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
 
 public class AnnualPayActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        getWindow().setSoftInputMode(
+                WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator_annual_pay);
     }
--- app/src/main/java/com/egr423/android/napkin/FinanceActivity.java
+++ app/src/main/java/com/egr423/android/napkin/FinanceActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.GridLayout;
 import android.widget.Toast;
 
