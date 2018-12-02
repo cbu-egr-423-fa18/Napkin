e6ca4a40304266f3696aaaebdf43d36c24ced2a8
Chris <chriscarrillo@outlook.com>
2018-12-01 22:37:42 -0800

    Started unit test for Annual Pay

---
+++ app/src/test/java/com/egr423/android/napkin/AnnualPayActivityTest.java
@@ -0,0 +1,18 @@
+package com.egr423.android.napkin;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AnnualPayActivityTest {
+
+    @Test
+    public void expectedHourlyRateTest() {
+
+    }
+
+    @Test
+    public void expectedAnnualPayTest() {
+
+    }
+}
